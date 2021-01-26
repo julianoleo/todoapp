6cb2ab607a90c330a9ec43616c42bce0e66eba6a
Juliano Leocádio Batista <julianoleo@gmail.com>
2021-01-26 12:42:46 -0300

    TodoAPP - 26/01/2021 12:42

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,3 +1,9 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
 import React from 'react'
 
-//teste juliano
+export default props => (
+    <div className='container'>
+    </div>
+)
