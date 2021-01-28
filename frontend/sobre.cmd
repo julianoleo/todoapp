021c89bfac63a066b8f00cc9a6dedfa611606fa1
Juliano Leocádio Batista <julianoleo@gmail.com>
2021-01-28 14:44:22 -0300

    TodoAPP - 28/01/2021 14:44h

---
+++ frontend/sobre.cmd
@@ -0,0 +1,12 @@
+@ECHO OFF.
+
+set data=%date:~0,2%/%date:~3,2%/%date:~6,4%
+set hora=%time:~0,2%:%time:~3,2%h
+
+@git add .
+@git commit -m "TodoAPP - %data% %hora%"
+@git push
+
+::echo Data atual: %data%
+::echo Hora Atual: %hora%
+
